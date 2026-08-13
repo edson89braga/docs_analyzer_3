bedb6e276766e6d6e7956040d865add94311e71a
Edson Braga <edson89braga@gmail.com>
2026-08-13 18:54:18 -0300

    FIX: Remove chamada órfã a check_app_version()

--- SOURCE/flet_ui/views/login_view.py
+++ SOURCE/flet_ui/views/login_view.py
@@ -272,11 +272,6 @@ def create_login_view(page: ft.Page) -> ft.View:
 
                 logger.info(f"Contexto do logger de nuvem atualizado para usuário {user_id}.")
 
-                from SOURCE.config.provider import is_local_mode
-                if is_local_mode():
-                    from SOURCE.utils import check_app_version
-                    check_app_version()
-
                 page.go("/home")
 
             else: # Falha na autenticação
